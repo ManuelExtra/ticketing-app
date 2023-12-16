c09a93a7831146afa18f8eddcc3aba7483108069
manuelExtra <olaleyeemmanuel23@gmail.com>
2023-12-16 20:47:51 +0100

    added test suites for the routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,5 @@
-import express from "express";
-import 'express-async-errors';
-import {json} from "body-parser";
-import { signupRouter } from "./routes/signup";
-import { signinRouter } from "./routes/signin";
-import { currentUserRouter } from "./routes/current-user";
-import { signoutRouter } from "./routes/signout";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
+import { app } from "./app";
 import mongoose from "mongoose";
-import cookieSession from 'cookie-session'
-
-const app = express();
-
-app.set('trust proxy', true) // traffics are being proxied to our server through ingress nginx
-app.use(json());
-app.use(
-    cookieSession({
-        signed: false, // disable encryption
-        secure: true // for https connection
-    })
-)
-
-app.use(signupRouter)
-app.use(signinRouter)
-app.use(currentUserRouter)
-app.use(signoutRouter)
-
-app.all('*', async () => {
-    throw new NotFoundError()
-})
-
-app.use(errorHandler)
-
 
 const start = async () => {
 
