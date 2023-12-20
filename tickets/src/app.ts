41dc9b5648f1f4a35d7d64172199bfd5235b8a10
manuelExtra <olaleyeemmanuel23@gmail.com>
2023-12-20 15:18:46 +0100

    added tests and routes for ticket service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,26 +1,35 @@
 import express from "express";
-import 'express-async-errors';
-import {json} from "body-parser";
+import "express-async-errors";
+import { json } from "body-parser";
 
-import { errorHandler, NotFoundError } from "@mxticketing/common";
-import cookieSession from 'cookie-session'
+import { currentUser, errorHandler, NotFoundError } from "@mxticketing/common";
+import cookieSession from "cookie-session";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 
-app.set('trust proxy', true) // traffics are being proxied to our server through ingress nginx
+app.set("trust proxy", true); // traffics are being proxied to our server through ingress nginx
 app.use(json());
 app.use(
-    cookieSession({
-        signed: false, // disable encryption
-        secure: process.env.NODE_ENV !== 'test' // for https connection
-    })
-)
+  cookieSession({
+    signed: false, // disable encryption
+    secure: process.env.NODE_ENV !== "test", // for https connection
+  })
+);
 
 
-app.all('*', async () => {
-    throw new NotFoundError()
-})
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
-app.use(errorHandler)
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
 
-export {app}
+app.use(errorHandler);
+
+export { app };
