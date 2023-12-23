0ded26a7af38cd2eb93ad7a19c62e635c5cf19a1
manuelExtra <olaleyeemmanuel23@gmail.com>
2023-12-23 01:19:03 +0100

    added nats event files to the @mxticketing/common package

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -6,7 +6,7 @@ const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher connected to NATS');
     
     // const data = JSON.stringify({
@@ -19,9 +19,14 @@ stan.on('connect', () => {
     //     console.log('Event published');
     // })
     const publisher = new TicketCreatedPublisher(stan);
-    publisher.publish({
-        id: '123',
-        title: 'concert',
-        price: 20
-    })
+    try {
+        
+        await publisher.publish({
+            id: '123',
+            title: 'concert',
+            price: 20
+        })
+    } catch (error) {
+        console.error(error)
+    }
 })
